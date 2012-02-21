a31f0192787011fd1d33cf56ce3938c915002a69
Kyle Diedrick <corporealshift@gmail.com>
2012-02-21 17:44:49 -0600

    Initial Commit

---
+++ Dice/GUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GUAppDelegate.h
+//  Dice
+//
+//  Created by Kyle Diedrick on 2/21/12.
+//  Copyright (c) 2012 Graphics Unplugged. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dice
+//
+//  Created by Kyle Diedrick on 2/21/12.
+//  Copyright (c) 2012 Graphics Unplugged. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GUAppDelegate class]));
+    }
+}
